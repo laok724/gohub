94a6e50c64f5d2a38a1bfd519c49efb4795d609c
laoko <siwangjianzun@gmail.com>
2022-09-26 18:32:14 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,38 +1,18 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gohub/bootstrap"
 )
 
 func main() {
-	//  new 一个 Gin Engine 实例
-	r := gin.New()
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "world",
-		})
-	})
-	// 处理404请求
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "页面返回404")
-		} else {
-			// 默认返回JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code": 404,
-				"error_msg":  "路由未定义，请确认url和请求方法是否正确。",
-			})
-		}
-
-	})
-	// 运行服务，指定监听端口
-	r.Run(":8000")
+	router := gin.New()
+	bootstrap.SetupRoute(router)
+	err := router.Run(":3000")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 }
