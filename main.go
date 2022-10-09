af374f7ccbeb207bc466eacec084b64d9c2ba4dc
laoko <siwangjianzun@gmail.com>
2022-10-09 15:59:00 +0800

    初始化数据库

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ func main() {
 	flag.StringVar(&env, "env", "", "加载.env文件，如--env=testing加载的是.env.testing文件")
 	flag.Parse()
 	config.InitConfig(env)
-
+	// 初始化DB
+	bootstrap.SetupDB()
 	router := gin.New()
 	bootstrap.SetupRoute(router)
 	err := router.Run(":" + config.Get("app.port"))
