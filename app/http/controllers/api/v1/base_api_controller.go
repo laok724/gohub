5f2d372aef904c4bcfac13219376b0c2529e4f96
laoko <siwangjianzun@gmail.com>
2022-10-10 14:33:44 +0800

    手机号是否已注册接口

---
+++ app/http/controllers/api/v1/base_api_controller.go
@@ -0,0 +1,7 @@
+package v1
+
+// 处理业务逻辑，gohub控制器v1
+
+// BaseAPIController基础控制器
+type BaseAPIController struct {
+}
--- bootstrap/database.go
+++ bootstrap/database.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"gohub/app/models/user"
 	"gohub/pkg/database"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm/logger"
@@ -16,32 +17,32 @@ import (
 
 // SetupDB 初始化数据库和ORM
 
-func SetupDB{
+func SetupDB() {
 	var dbConfig gorm.Dialector
 	switch config.Get("database.connection") {
-	case mysql:
+	case "mysql":
 		// 构建DSN信息
-		dsn:=fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&multiStatements=true&loc=Local",
+		dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&multiStatements=true&loc=Local",
 			config.Get("database.mysql.username"),
 			config.Get("database.mysql.password"),
 			config.Get("database.mysql.host"),
 			config.Get("database.mysql.port"),
 			config.Get("database.mysql.database"),
 			config.Get("database.mysql.charset"),
-			)
-		dbConfig=mysql.New(mysql.Config{
-			DSN:dsn,
+		)
+		dbConfig = mysql.New(mysql.Config{
+			DSN: dsn,
 		})
 	case "sqlite":
 		// 初始化sqlite
-		database:=config.Get("database.sqlite.database")
-		dbConfig=sqlite.Open(database)
+		database := config.Get("database.sqlite.database")
+		dbConfig = sqlite.Open(database)
 	default:
 		panic(errors.New("database connection not supported"))
 	}
 
 	// 连接数据库，并设置GORM的日志模式
-	database.Connect(dbConfig,logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
@@ -50,5 +51,6 @@ func SetupDB{
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 
 	// 设置每个连接过期时间
-	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds"))*time.Seconds)
+	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+	database.DB.AutoMigrate(&user.User{})
 }
